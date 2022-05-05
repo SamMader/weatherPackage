14161c0ffcae0956e6a00247cf711c0217955cb4
Sam Mader <madersr@miamioh.edu>
2022-05-05 16:38:22 -0400

    Initial commit

---
+++ src/Hello.php
@@ -0,0 +1,20 @@
+<?php 
+
+/*
+ * (c) Your Name <madersr@miamioh.edu>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace madersr\weatherPackage;
+
+class Hello
+{
+    public function say($name)
+    {
+        return "Hello, $name!";
+    }
+}
+
+?>
